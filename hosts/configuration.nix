148dc130061dd34f612621deca215fbd11d729fb
devrtc0 <azat.mirzaianov@gmail.com>
2022-08-28 18:56:37 +0400

    add packages

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -1,9 +1,14 @@
-{ lib, ... }: {
+{ lib, pkgs, ... }: {
   time = {
     timeZone = "Europe/Samara";
   };
   system = {
     stateVersion = "22.11";
   };
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  environment = {
+    systemPackages = with pkgs; [
+      git
+      slack
+    ];
+  };
 }
--- hosts/default.nix
+++ hosts/default.nix
@@ -1,9 +1,12 @@
 { nixpkgs, ... }:
 let
   lib = nixpkgs.lib;
+  system = "x86_64-linux";
+
 in
 {
   vm = lib.nixosSystem {
+    inherit system;
     modules = [
       ./configuration.nix
       ./vm
