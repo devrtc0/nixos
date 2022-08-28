d9da537c40fabcd3e3a9ce2690ec5f86256a9820
devrtc0 <azat.mirzaianov@gmail.com>
2022-08-29 00:15:50 +0400

    initial password

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -7,7 +7,7 @@
         version = "22.7.1.828-1";
         meta.knownVulnerabilities = [ ];
         src = super.fetchurl {
-          url = "https://repo.yandex.ru/yandex-browser/deb/pool/main/y/yandex-browser-beta/yandex-browser-beta_22.7.1.828-1_amd64.deb";
+          url = super.url;
           sha256 = "11510b5f46a170b5da831c14870ab1e183afe65635cc7835258519ec0164c7d7";
         };
       });
@@ -69,6 +69,7 @@
     defaultUserShell = pkgs.fish;
     users.${user} = {
       isNormalUser = true;
+      initialPassword = " ";
       extraGroups = [ "wheel" "video" "audio" "camera" "networkmanager" "disk" ];
     };
   };
