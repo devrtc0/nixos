e6c0715a610ebd5b7b305dc8176a6ac1bf285750
devrtc0 <azat.mirzaianov@gmail.com>
2022-08-29 13:12:23 +0400

    yandex browser

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -5,10 +5,14 @@
     (self: super: {
       yandex-browser = super.yandex-browser.overrideAttrs (old: rec {
         version = "22.7.1.828-1";
+        pname = old.pname;
         meta.knownVulnerabilities = [ ];
-        src = super.fetchurl {
-          url = "http://repo.yandex.ru/yandex-browser/deb/pool/main/y/${old.pname}-beta/${old.pname}-beta_${version}_amd64.deb";
+        _src = {
+          url = "http://repo.yandex.ru/yandex-browser/deb/pool/main/y/${pname}-beta/${pname}-beta_${version}_amd64.deb";
           sha256 = "11510b5f46a170b5da831c14870ab1e183afe65635cc7835258519ec0164c7d7";
+        };
+        src = super.fetchurl {
+          inherit _src;
         };
       });
     })
