b875899a075f40fd6ead109285d23c12b494854e
devrtc0 <azat.mirzaianov@gmail.com>
2022-09-04 23:47:18 +0400

    home-manager

--- hosts/default.nix
+++ hosts/default.nix
@@ -19,11 +19,13 @@ in
 
       home-manager.nixosModules.home-manager
       {
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.extraSpecialArgs = { inherit user version; };
-        home-manager.users.${user} = {
-          imports = [ (import ./home.nix) ] ++ [ (import ./vm/home.nix) ];
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          extraSpecialArgs = { inherit user version; };
+          users.${user} = {
+            imports = [ (import ./home.nix) ] ++ [ (import ./vm/home.nix) ];
+          };
         };
       }
     ];
