3ffec4926d6de98b3397810335ae5d69fd033fe7
devrtc0 <me@devrtc0.ru>
2022-09-16 19:07:25 +0400

    lenovo host

---
+++ hosts/t15g1/disks.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+check() {
+	if [ $? -ne 0 ]; then
+		printf "${RED}ERROR${NC}"
+		exit -1
+	fi
+}
+
+set -x
+
+umount -qR /mnt
+
+DEVICE="/dev/nvme0n1"
+
+sgdisk --zap-all $DEVICE
+check
+sgdisk -o $DEVICE
+check
+sgdisk -n 1:0:+256M -t 1:ef00 -N 2 -t 2:8300 $DEVICE
+check
+
+sync
+
+BOOT_DEVICE=$(lsblk -p -n -o NAME -x NAME $DEVICE | head -2 | tail -1)
+ROOT_DEVICE=$(lsblk -p -n -o NAME -x NAME $DEVICE | tail -1)
+
+yes | mkfs.fat -n boot -F32 "$BOOT_DEVICE"
+check
+yes | mkfs.ext4 -L nixos "$ROOT_DEVICE"
+check
+
+sync
+
+mount /dev/disk/by-label/nixos /mnt
+check
+mkdir -p /mnt/boot
+check
+mount /dev/disk/by-label/boot /mnt/boot
+check
+
+set +x
---
+++ hosts/t15g1/hardware.nix
@@ -0,0 +1,39 @@
+{ lib, config, ... }:
+
+{
+  imports = [ ];
+
+  boot = {
+    initrd = {
+      availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "usbhid" "sr_mod" ];
+      kernelModules = [ ];
+    };
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+    loader = {
+      systemd-boot = {
+        enable = true;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+      };
+    };
+  };
+
+  fileSystems = {
+    "/" =
+      {
+        device = "/dev/disk/by-label/nixos";
+        fsType = "ext4";
+      };
+    "/boot" =
+      {
+        device = "/dev/disk/by-label/boot";
+        fsType = "vfat";
+      };
+  };
+
+  swapDevices = [ ];
+
+  hardware.cpu.intel.updateMicrocode = true;
+}
